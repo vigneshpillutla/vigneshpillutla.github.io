6c3c1d03208c03ecd32c5f631625d7738f1a5571
Vignesh Pillutla <vigneshpillutla@gmail.com>
2023-10-14 13:31:02 +0530

    Added hero section

---
+++ index.js
@@ -0,0 +1,4 @@
+// Elements
+const navbar = document.getElementById("navbar");
+const heroSection = document.getElementById('hero');
+heroSection.style.height = `${window.innerHeight - navbar.offsetHeight}px`;
